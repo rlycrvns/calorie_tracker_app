91358a2703b518113b533f0926c0f5cbb31a1fa2
rlycrvns <rileycravens@Rileys-MacBook-Pro.local>
2020-09-15 11:19:11 -0700

    add: controller definitions/data

---
+++ app.js
@@ -0,0 +1,28 @@
+// Storage Controller
+
+// Item Controller
+const ItemCtrl = (function () {
+  // Item Constructor
+  const Item = function (id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  };
+
+  // Date Structure / State
+  const data = {
+    items: [
+      { id: 0, name: 'Steak Dinner', calories: 1200 },
+      { id: 1, name: 'Cookie', calories: 400 },
+      { id: 2, name: 'Eggs', calories: 300 },
+    ],
+    currentItem: null,
+    totalCalories: 0,
+  };
+})();
+
+// UI Controller
+const UICtrl = (function () {})();
+
+// App Controller
+const AppCtrl = (function (ItemCtrl, UICtrl) {})(ItemCtrl, UICtrl);
